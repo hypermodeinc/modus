29b372b8d6b5375f25df380c3d7ef416c3da8881
Matt Johnson-Pint <mjp@hypermode.com>
2023-11-03 12:25:50 -0700

    Update plugin example

--- plugins/as/hmplugin1/assembly/index.ts
+++ plugins/as/hmplugin1/assembly/index.ts
@@ -8,34 +8,26 @@ export function getFullName(firstName: string, lastName: string): string {
   return `${firstName} ${lastName}`;
 }
 
-
-// This would be ideal, as it returns an array of Person objects.
-// However, it doesn't work because we don't yet know how to read the objects on the host.
-// export function getPeople(): Person[] {
-//   return [
-//     new Person("Bob", "Smith"),
-//     new Person("Alice", "Jones")
-//   ];
-// }
-
 export function getPeople(): string {
   const people = [
     new Person("Bob", "Smith"),
     new Person("Alice", "Jones")
   ];
 
-  // For now, we have to return complex data as JSON.
+  // Non-scalar values must be returned as JSON.
   return JSON.stringify(people);
 }
 
 // @ts-ignore
 @json
 class Person {
-  constructor(public firstName: string, public lastName: string) {}
-
-  // TODO: This works in AssemblyScript, but it doesn't return in the JSON,
-  // and Dgraph doesn't resolve back to a new function call either.
-  get fullName(): string {
-    return `${this.firstName} ${this.lastName}`;
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.fullName = getFullName(firstName, lastName);
   }
+  
+  firstName: string;
+  lastName: string;
+  fullName: string;
 };
