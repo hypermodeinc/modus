f9849a18183008f14dc09c103d3a12d73e420e5c
Matt Johnson-Pint <mjp@hypermode.com>
2024-01-08 18:49:15 -0800

    More organization

---
+++ hmruntime/fncall.go
@@ -0,0 +1,180 @@
+/*
+ * Copyright 2023 Hypermode, Inc.
+ */
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	"github.com/dgraph-io/gqlparser/ast"
+	wasm "github.com/tetratelabs/wazero/api"
+)
+
+func callFunction(ctx context.Context, mod wasm.Module, info functionInfo, inputs map[string]any) (any, error) {
+	fnName := info.FunctionName()
+	fn := mod.ExportedFunction(fnName)
+	def := fn.Definition()
+	paramTypes := def.ParamTypes()
+	schema := info.Schema
+
+	// Get parameters to pass as input to the plugin function
+	// Note that we can't use def.ParamNames() because they are only available when the plugin
+	// is compiled in debug mode. They're striped by optimization in release mode.
+	// Instead, we can use the argument names from the schema.
+	// Also note, that the order of the arguments from schema should match order of params in wasm.
+	params := make([]uint64, len(paramTypes))
+	for i, arg := range schema.FunctionArgs() {
+		val := inputs[arg.Name]
+		if val == nil {
+			return nil, fmt.Errorf("parameter %s is missing", arg.Name)
+		}
+
+		param, err := convertParam(ctx, mod, *arg.Type, paramTypes[i], val)
+		if err != nil {
+			return nil, fmt.Errorf("parameter %s is invalid: %w", arg.Name, err)
+		}
+
+		params[i] = param
+	}
+
+	// Call the wasm function
+	fmt.Printf("Calling function '%s' for resolver '%s'\n", fnName, schema.Resolver())
+	res, err := fn.Call(ctx, params...)
+	if err != nil {
+		return nil, err
+	}
+
+	// Get the result
+	mem := mod.Memory()
+	result, err := convertResult(mem, *schema.FieldDef.Type, def.ResultTypes()[0], res[0])
+	if err != nil {
+		return nil, fmt.Errorf("failed to convert result: %w", err)
+	}
+
+	return result, nil
+}
+
+func convertParam(ctx context.Context, mod wasm.Module, schemaType ast.Type, wasmType wasm.ValueType, val any) (uint64, error) {
+
+	switch schemaType.NamedType {
+
+	case "Boolean":
+		b, ok := val.(bool)
+		if !ok {
+			return 0, fmt.Errorf("input value is not a bool")
+		}
+
+		// Note, booleans are passed as i32 in wasm
+		if wasmType != wasm.ValueTypeI32 {
+			return 0, fmt.Errorf("parameter is not defined as a bool on the function")
+		}
+
+		if b {
+			return 1, nil
+		} else {
+			return 0, nil
+		}
+
+	case "Int":
+		n, err := val.(json.Number).Int64()
+		if err != nil {
+			return 0, fmt.Errorf("input value is not an int")
+		}
+
+		switch wasmType {
+		case wasm.ValueTypeI32:
+			return wasm.EncodeI32(int32(n)), nil
+		case wasm.ValueTypeI64:
+			return wasm.EncodeI64(n), nil
+		default:
+			return 0, fmt.Errorf("parameter is not defined as an int on the function")
+		}
+
+	case "Float":
+		n, err := val.(json.Number).Float64()
+		if err != nil {
+			return 0, fmt.Errorf("input value is not a float")
+		}
+
+		switch wasmType {
+		case wasm.ValueTypeF32:
+			return wasm.EncodeF32(float32(n)), nil
+		case wasm.ValueTypeF64:
+			return wasm.EncodeF64(n), nil
+		default:
+			return 0, fmt.Errorf("parameter is not defined as a float on the function")
+		}
+
+	case "String", "ID", "":
+		s, ok := val.(string)
+		if !ok {
+			return 0, fmt.Errorf("input value is not a string")
+		}
+
+		// Note, strings are passed as a pointer to a string in wasm memory
+		if wasmType != wasm.ValueTypeI32 {
+			return 0, fmt.Errorf("parameter is not defined as a string on the function")
+		}
+
+		ptr := writeString(ctx, mod, s)
+		return uint64(ptr), nil
+
+	default:
+		return 0, fmt.Errorf("unknown parameter type: %s", schemaType.NamedType)
+	}
+}
+
+func convertResult(mem wasm.Memory, schemaType ast.Type, wasmType wasm.ValueType, res uint64) (any, error) {
+
+	switch schemaType.NamedType {
+
+	case "Boolean":
+		if wasmType != wasm.ValueTypeI32 {
+			return nil, fmt.Errorf("return type is not defined as an bool on the function")
+		}
+
+		return res != 0, nil
+
+	case "Int":
+
+		// TODO: Do we need to handle unsigned ints differently?
+
+		switch wasmType {
+		case wasm.ValueTypeI32:
+			return wasm.DecodeI32(res), nil
+
+		case wasm.ValueTypeI64:
+			return int64(res), nil
+
+		default:
+			return nil, fmt.Errorf("return type is not defined as an int on the function")
+		}
+
+	case "Float":
+
+		switch wasmType {
+		case wasm.ValueTypeF32:
+			return wasm.DecodeF32(res), nil
+
+		case wasm.ValueTypeF64:
+			return wasm.DecodeF64(res), nil
+
+		default:
+			return nil, fmt.Errorf("return type is not defined as a float on the function")
+		}
+
+	case "ID":
+		return nil, fmt.Errorf("the ID scalar is not allowed for function return types (use String instead)")
+
+	default:
+		// The return type is either a string, or an object that should be serialized as JSON.
+		// Strings are passed as a pointer to a string in wasm memory
+		if wasmType != wasm.ValueTypeI32 {
+			return nil, fmt.Errorf("return type was not a pointer")
+		}
+
+		return readString(mem, uint32(res))
+	}
+}
---
+++ hmruntime/modmgmt.go
@@ -0,0 +1,123 @@
+/*
+ * Copyright 2023 Hypermode, Inc.
+ */
+package main
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+
+	"github.com/google/uuid"
+	"github.com/tetratelabs/wazero"
+	wasm "github.com/tetratelabs/wazero/api"
+)
+
+var runtime wazero.Runtime
+
+type buffers struct {
+	Stdout *strings.Builder
+	Stderr *strings.Builder
+}
+
+func initWasmRuntime(ctx context.Context) (wazero.Runtime, error) {
+
+	// Create the runtime
+	cfg := wazero.NewRuntimeConfig().
+		WithCloseOnContextDone(true)
+	runtime := wazero.NewRuntimeWithConfig(ctx, cfg)
+
+	// Connect WASI host functions
+	err := instantiateWasiFunctions(ctx, runtime)
+	if err != nil {
+		return nil, err
+	}
+
+	// Connect Hypermode host functions
+	err = instantiateHostFunctions(ctx, runtime)
+	if err != nil {
+		return nil, err
+	}
+
+	return runtime, nil
+}
+
+func loadPluginModule(ctx context.Context, name string) error {
+	cmOld, reloading := compiledModules[name]
+	if reloading {
+		fmt.Printf("Reloading plugin '%s'\n", name)
+	} else {
+		fmt.Printf("Loading plugin '%s'\n", name)
+	}
+
+	path, err := getPathForPlugin(name)
+	if err != nil {
+		return fmt.Errorf("failed to get path for plugin: %w", err)
+	}
+
+	plugin, err := os.ReadFile(path)
+	if err != nil {
+		return fmt.Errorf("failed to load the plugin: %w", err)
+	}
+
+	// Compile the plugin into a module.
+	cm, err := runtime.CompileModule(ctx, plugin)
+	if err != nil {
+		return fmt.Errorf("failed to compile the plugin: %w", err)
+	}
+
+	// Store the compiled module for later retrieval.
+	compiledModules[name] = cm
+
+	// When reloading, close the old module.
+	if reloading {
+		cmOld.Close(ctx)
+	}
+
+	return nil
+}
+
+func unloadPluginModule(ctx context.Context, name string) error {
+	cmOld, found := compiledModules[name]
+	if !found {
+		return fmt.Errorf("plugin not found '%s'", name)
+	}
+
+	fmt.Printf("Unloading plugin '%s'\n", name)
+	delete(compiledModules, name)
+	cmOld.Close(ctx)
+
+	return nil
+}
+
+func getModuleInstance(ctx context.Context, pluginName string) (wasm.Module, buffers, error) {
+
+	// Create string buffers to capture stdout and stderr.
+	// Still write to the console, but also capture the output in the buffers.
+	buf := buffers{&strings.Builder{}, &strings.Builder{}}
+	wOut := io.MultiWriter(os.Stdout, buf.Stdout)
+	wErr := io.MultiWriter(os.Stderr, buf.Stderr)
+
+	// Get the compiled module.
+	compiled, ok := compiledModules[pluginName]
+	if !ok {
+		return nil, buf, fmt.Errorf("no compiled module found for plugin '%s'", pluginName)
+	}
+
+	// Configure the module instance.
+	cfg := wazero.NewModuleConfig().
+		WithName(pluginName + "_" + uuid.NewString()).
+		WithStdout(wOut).WithStderr(wErr)
+
+	// Instantiate the plugin as a module.
+	// NOTE: This will also invoke the plugin's `_start` function,
+	// which will call any top-level code in the plugin.
+	mod, err := runtime.InstantiateModule(ctx, compiled, cfg)
+	if err != nil {
+		return nil, buf, fmt.Errorf("failed to instantiate the plugin module: %w", err)
+	}
+
+	return mod, buf, nil
+}
