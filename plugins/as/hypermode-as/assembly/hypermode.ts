fe1b01a9ca6df474df8b4f3c411b5386f03beaca
Matt Johnson-Pint <mjp@hypermode.com>
2024-01-11 14:48:33 -0800

    Delete plugins folder

--- plugins/as/hypermode-as/assembly/hypermode.ts
+++
@@ -1,4 +0,0 @@
-// This file should only export functions from the "hypermode" host module.
-
-export declare function executeDQL(statement: string, isMutation: bool): string;
-export declare function executeGQL(statement: string): string;
