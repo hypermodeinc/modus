a587c3ac1120f057156df2e2657eca4de6605cde
Jai Radhakrishnan <55522316+jairad26@users.noreply.github.com>
2024-03-15 12:57:40 -0700

    only log errors once, warn schema loading error (#107)
    * only log errors once, warn schema loading error
    
    * fixes

--- functions/schemaWatcher.go
+++ functions/schemaWatcher.go
@@ -24,16 +24,21 @@ func MonitorGqlSchema(ctx context.Context) {
 		ticker := time.NewTicker(config.RefreshInterval)
 		defer ticker.Stop()
 
+		var urlErrorLogged, schemaErrorLogged bool
 		for {
 			schema, err := dgraph.GetGQLSchema(ctx)
 			if err != nil {
 				var urlErr *url.Error
-				if errors.As(err, &urlErr) {
+				if errors.As(err, &urlErr) && !urlErrorLogged {
 					logger.Err(ctx, urlErr).Msg("Failed to connect to Dgraph.")
-				} else {
+					urlErrorLogged = true
+				} else if !schemaErrorLogged {
 					logger.Err(ctx, err).Msg("Failed to retrieve GraphQL schema.")
+					schemaErrorLogged = true
 				}
 			} else if schema != gqlSchema {
+				urlErrorLogged = false
+				schemaErrorLogged = false
 				if gqlSchema == "" {
 					logger.Info(ctx).Msg("Schema loaded.")
 				} else {
