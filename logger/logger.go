3b3a10d9d542ba01fa02a07c2c1b6c2d5363d20c
Matt Johnson-Pint <mjp@hypermode.com>
2024-03-20 11:28:30 -0700

    Get plugin and build_id from context

--- logger/logger.go
+++ logger/logger.go
@@ -18,7 +18,12 @@ import (
 type contextKey string
 
 const executionIdKey = "execution_id"
+const pluginNameKey = "plugin"
+const buildIdKey = "build_id"
+
 const ExecutionIdContextKey contextKey = executionIdKey
+const PluginNameContextKey contextKey = pluginNameKey
+const BuildIdContextKey contextKey = buildIdKey
 
 func Initialize() *zerolog.Logger {
 	if config.UseJsonLogging {
@@ -42,13 +47,33 @@ func Initialize() *zerolog.Logger {
 }
 
 func Get(ctx context.Context) *zerolog.Logger {
-	executionId, ok := ctx.Value(ExecutionIdContextKey).(string)
-	if ok && executionId != "" {
-		l := log.Logger.With().Str(executionIdKey, executionId).Logger()
-		return &l
+	executionId, found1 := ctx.Value(ExecutionIdContextKey).(string)
+	buildId, found2 := ctx.Value(BuildIdContextKey).(string)
+	pluginName, found3 := ctx.Value(PluginNameContextKey).(string)
+
+	// If no context values, just return the global logger.
+	if !found1 && !found2 && !found3 {
+		return &log.Logger
 	}
 
-	return &log.Logger
+	// Create a logger context with the context values.
+	lc := log.Logger.With()
+
+	if executionId != "" {
+		lc = lc.Str(executionIdKey, executionId)
+	}
+
+	if buildId != "" {
+		lc = lc.Str(buildIdKey, buildId)
+	}
+
+	if pluginName != "" {
+		lc = lc.Str(pluginNameKey, pluginName)
+	}
+
+	// Return a logger built from the context.
+	l := lc.Logger()
+	return &l
 }
 
 func Trace(ctx context.Context) *zerolog.Event {
