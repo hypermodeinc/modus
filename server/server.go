2be96ae2d6447bd21f198142ac2c1d6fcff365f5
Matt Johnson-Pint <mjp@hypermode.com>
2024-04-15 14:56:30 -0700

    Temporarily remove lambda support

--- server/server.go
+++ server/server.go
@@ -11,7 +11,6 @@ import (
 
 	"hmruntime/config"
 	"hmruntime/graphql"
-	"hmruntime/lambda"
 	"hmruntime/logger"
 )
 
@@ -20,7 +19,7 @@ func Start(ctx context.Context) error {
 		Int("port", config.Port).
 		Msg("Listening for incoming requests.")
 	http.HandleFunc("/graphql", graphql.HandleGraphQLRequest)
-	http.HandleFunc("/graphql-worker", lambda.HandleDgraphLambdaRequest)
+	// http.HandleFunc("/graphql-worker", lambda.HandleDgraphLambdaRequest)
 	http.HandleFunc("/admin", handleAdminRequest)
 	return http.ListenAndServe(fmt.Sprintf(":%d", config.Port), nil)
 }
