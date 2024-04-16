3a152b6b713f89f861888b630afce8aa787e4b05
Matt Johnson-Pint <mjp@hypermode.com>
2024-04-16 10:38:16 -0700

    Remove extraneous type registration

--- functions/registration.go
+++ functions/registration.go
@@ -13,16 +13,10 @@ import (
 )
 
 var Functions = make(map[string]FunctionInfo)
-var TypeDefinitions = make(map[string]TypeInfo)
 
 type FunctionInfo struct {
 	Function plugins.FunctionSignature
 	Plugin   *plugins.Plugin
-}
-
-type TypeInfo struct {
-	Type   plugins.TypeDefinition
-	Plugin *plugins.Plugin
 }
 
 func MonitorRegistration(ctx context.Context) {
@@ -75,21 +69,6 @@ func (r *registration) registerPlugin(ctx context.Context, plugin *plugins.Plugi
 			Str("build_id", plugin.BuildId()).
 			Msg("Registered function.")
 	}
-
-	// Save types from the metadata to the type definitions map
-	for _, td := range plugin.Metadata.Types {
-		TypeDefinitions[td.Name] = TypeInfo{
-			Type:   td,
-			Plugin: plugin,
-		}
-		r.types[td.Name] = true
-
-		logger.Info(ctx).
-			Str("type", td.Name).
-			Str("plugin", plugin.Name()).
-			Str("build_id", plugin.BuildId()).
-			Msg("Registered custom data type.")
-	}
 }
 
 func (r *registration) cleanup(ctx context.Context) {
@@ -103,18 +82,6 @@ func (r *registration) cleanup(ctx context.Context) {
 				Str("plugin", fn.Plugin.Name()).
 				Str("build_id", fn.Plugin.BuildId()).
 				Msg("Unregistered function.")
-		}
-	}
-
-	// Cleanup any previously registered types
-	for name, fn := range TypeDefinitions {
-		if !r.types[name] {
-			delete(TypeDefinitions, name)
-			logger.Info(ctx).
-				Str("type", name).
-				Str("plugin", fn.Plugin.Name()).
-				Str("build_id", fn.Plugin.BuildId()).
-				Msg("Unregistered custom data type.")
 		}
 	}
 }
