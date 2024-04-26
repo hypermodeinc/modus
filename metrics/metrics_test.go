25d5eb779ed25ce863a72579e394c27c628e67e0
Matt Johnson-Pint <mjp@hypermode.com>
2024-04-26 12:13:59 -0700

    Add graceful shutdown for HTTP server (#153)

--- metrics/metrics_test.go
+++ metrics/metrics_test.go
@@ -6,59 +6,34 @@ package metrics_test
 
 import (
 	"bytes"
-	"context"
-	"fmt"
-	"hmruntime/config"
-	"hmruntime/server"
 	"io"
 	"net/http"
+	"net/http/httptest"
 	"path"
 	"runtime"
 	"testing"
-	"time"
+
+	"hmruntime/config"
+	"hmruntime/server"
 
 	"github.com/prometheus/common/expfmt"
 )
 
 const (
-	configPort = 8080
+	graphqlEndpoint = "/graphql"
+	adminEndpoint   = "/admin"
+	healthEndpoint  = "/health"
+	metricsEndpoint = "/metrics"
 )
 
-var (
-	graphqlEndpoint = fmt.Sprintf("http://localhost:%d/graphql", configPort)
-	adminEndpoint   = fmt.Sprintf("http://localhost:%d/admin", configPort)
-	healthEndpoint  = fmt.Sprintf("http://localhost:%d/health", configPort)
-	metricsEndpoint = fmt.Sprintf("http://localhost:%d/metrics", configPort)
-)
-
-func setupRuntime(t *testing.T) {
-	// configure the port to listen on
-	config.Port = configPort
-
+func setupRuntime() {
 	// configure a wasm plugin
 	_, thisFilePath, _, _ := runtime.Caller(0)
 	config.StoragePath = path.Join(thisFilePath, "..", "testutil", "data", "test-as")
-
-	go func() {
-		if err := server.Start(context.Background()); err != nil {
-			t.Logf("error while shutting down the server: %v", err)
-		}
-	}()
-
-	for i := 0; i < 10; i++ {
-		resp, err := http.Get(metricsEndpoint)
-		if err == nil {
-			resp.Body.Close()
-			break
-		}
-		time.Sleep(1 * time.Second)
-	}
-
-	// It is possible that server did not come up but we have tried enough times.
 }
 
-func httpGet(t *testing.T, url string) []byte {
-	resp, err := http.Get(url)
+func httpGet(t *testing.T, s *httptest.Server, endpoint string) []byte {
+	resp, err := http.Get(s.URL + endpoint)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -70,8 +45,8 @@ func httpGet(t *testing.T, url string) []byte {
 	return data
 }
 
-func ensureValidMetrics(t *testing.T, totalRequests int) {
-	metricsOutput := httpGet(t, metricsEndpoint)
+func ensureValidMetrics(t *testing.T, s *httptest.Server, totalRequests int) {
+	metricsOutput := httpGet(t, s, metricsEndpoint)
 
 	var parser expfmt.TextParser
 	mf, err := parser.TextToMetricFamilies(bytes.NewReader(metricsOutput))
@@ -86,14 +61,19 @@ func ensureValidMetrics(t *testing.T, totalRequests int) {
 }
 
 func TestRuntimeMetrics(t *testing.T) {
-	setupRuntime(t)
 
-	_ = httpGet(t, adminEndpoint)
-	ensureValidMetrics(t, 1)
+	setupRuntime()
 
-	_ = httpGet(t, graphqlEndpoint)
-	ensureValidMetrics(t, 2)
+	mux := server.GetHandlerMux()
+	s := httptest.NewServer(mux)
+	defer s.Close()
 
-	_ = httpGet(t, healthEndpoint)
-	ensureValidMetrics(t, 2)
+	_ = httpGet(t, s, adminEndpoint)
+	ensureValidMetrics(t, s, 1)
+
+	_ = httpGet(t, s, graphqlEndpoint)
+	ensureValidMetrics(t, s, 2)
+
+	_ = httpGet(t, s, healthEndpoint)
+	ensureValidMetrics(t, s, 2)
 }
