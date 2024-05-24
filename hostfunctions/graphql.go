92dfed83c4a0cc7ef3fbb6948dbe4c5abd3a3735
Matt Johnson-Pint <mjp@hypermode.com>
2024-05-24 11:42:22 -0700

    Refactoring

--- hostfunctions/graphql.go
+++ hostfunctions/graphql.go
@@ -6,13 +6,15 @@ package hostfunctions
 
 import (
 	"context"
+	"fmt"
 
-	"hmruntime/connections"
 	"hmruntime/functions/assemblyscript"
 	"hmruntime/hosts"
 	"hmruntime/logger"
 	"hmruntime/utils"
 
+	"github.com/buger/jsonparser"
+	"github.com/hypermodeAI/manifest"
 	wasm "github.com/tetratelabs/wazero/api"
 )
 
@@ -37,7 +39,7 @@ func hostExecuteGQL(ctx context.Context, mod wasm.Module, pHostName uint32, pStm
 		return 0
 	}
 
-	result, err := connections.ExecuteGraphqlApi(ctx, host, stmt, vars)
+	result, err := executeGraphql(ctx, host, stmt, vars)
 	if err != nil {
 		logger.Err(ctx, err).Msg("Error executing GraphQL operation.")
 		return 0
@@ -50,4 +52,48 @@ func hostExecuteGQL(ctx context.Context, mod wasm.Module, pHostName uint32, pStm
 	}
 
 	return offset
+}
+
+type graphqlRequest struct {
+	Query     string         `json:"query"`
+	Variables map[string]any `json:"variables"`
+}
+
+func executeGraphql(ctx context.Context, host manifest.HostInfo, stmt string, vars map[string]any) (string, error) {
+	request := graphqlRequest{
+		Query:     stmt,
+		Variables: vars,
+	}
+
+	headers := map[string]string{}
+
+	if host.Endpoint == "" {
+		return "", fmt.Errorf("host endpoint is not defined")
+	}
+	if host.AuthHeader != "" {
+		key, err := hosts.GetHostSecret(ctx, host, host.AuthHeader)
+		if err != nil {
+			return "", err
+		}
+
+		headers[host.AuthHeader] = key
+	}
+
+	response, err := utils.PostHttp[[]byte](host.Endpoint, request, headers)
+	if err != nil {
+		return "", fmt.Errorf("error posting GraphQL statement: %w", err)
+	}
+
+	gqlErrors, dataType, _, err := jsonparser.Get(response, "errors")
+	if err != nil && err != jsonparser.KeyPathNotFoundError {
+		return "", fmt.Errorf("error parsing GraphQL response: %w", err)
+	}
+	if dataType == jsonparser.Array && len(gqlErrors) > 0 {
+		logger.Warn(ctx).
+			Bool("user_visible", true).
+			Str("errors", string(gqlErrors)).
+			Msg("GraphQL API call returned errors.")
+	}
+
+	return string(response), nil
 }
