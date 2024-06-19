1796168e3b5eac181b123dcb4bc559960f1b45a0
Matt Johnson-Pint <mjp@hypermode.com>
2024-06-18 17:56:20 -0700

    .

--- hosts/hosts.go
+++ hosts/hosts.go
@@ -69,7 +69,6 @@ func PostToHostEndpoint[TResult any](ctx context.Context, host manifest.HostInfo
 	}
 
 	bs := func(ctx context.Context, req *http.Request) error {
-		req.Header.Set("Content-Type", "application/json")
 		return secrets.ApplyHostSecrets(ctx, host, req)
 	}
 
