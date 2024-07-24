cecdf52dd282e1e1a586e692a7cd889d69f5df3d
Matt Johnson-Pint <mjp@hypermode.com>
2024-07-24 12:58:53 -0700

    Refactor graphql host function (#294)

---
+++ graphqlclient/graphqlclient.go
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2024 Hypermode, Inc.
+ */
+
+package graphqlclient
+
+import (
+	"context"
+	"fmt"
+
+	"hmruntime/hosts"
+	"hmruntime/logger"
+	"hmruntime/utils"
+
+	"github.com/buger/jsonparser"
+)
+
+type graphqlRequestPayload struct {
+	Query     string         `json:"query"`
+	Variables map[string]any `json:"variables"`
+}
+
+func Execute(ctx context.Context, hostName string, stmt string, varsJson string) (string, error) {
+
+	host, err := hosts.GetHttpHost(hostName)
+	if err != nil {
+		return "", err
+	}
+
+	vars := make(map[string]any)
+	if err := utils.JsonDeserialize([]byte(varsJson), &vars); err != nil {
+		return "", err
+	}
+
+	// https://graphql.org/learn/serving-over-http/
+	payload := graphqlRequestPayload{
+		Query:     stmt,
+		Variables: vars,
+	}
+
+	result, err := hosts.PostToHostEndpoint[[]byte](ctx, host, payload)
+	if err != nil {
+		return "", fmt.Errorf("error posting GraphQL statement: %w", err)
+	}
+
+	// Check for errors in the response so we can log them.
+	response := result.Data
+	gqlErrors, dataType, _, err := jsonparser.Get(response, "errors")
+	if err != nil && err != jsonparser.KeyPathNotFoundError {
+		return "", fmt.Errorf("error parsing GraphQL response: %w", err)
+	}
+	if dataType == jsonparser.Array && len(gqlErrors) > 0 {
+		logger.Warn(ctx).
+			Bool("user_visible", true).
+			Str("errors", string(gqlErrors)).
+			Msg("GraphQL API call returned errors.")
+	}
+
+	return string(response), nil
+}
--- hostfunctions/graphql.go
+++ hostfunctions/graphql.go
@@ -6,39 +6,23 @@ package hostfunctions
 
 import (
 	"context"
-	"fmt"
 
-	"hmruntime/hosts"
+	"hmruntime/graphqlclient"
 	"hmruntime/logger"
-	"hmruntime/utils"
 
-	"github.com/buger/jsonparser"
-	"github.com/hypermodeAI/manifest"
 	wasm "github.com/tetratelabs/wazero/api"
 )
 
-func hostExecuteGQL(ctx context.Context, mod wasm.Module, pHostName uint32, pStmt uint32, pVars uint32) uint32 {
+func hostExecuteGQL(ctx context.Context, mod wasm.Module, pHostName uint32, pStmt uint32, pVarsJson uint32) uint32 {
 
-	var hostName, stmt, sVars string
-	err := readParams3(ctx, mod, pHostName, pStmt, pVars, &hostName, &stmt, &sVars)
+	var hostName, stmt, varsJson string
+	err := readParams3(ctx, mod, pHostName, pStmt, pVarsJson, &hostName, &stmt, &varsJson)
 	if err != nil {
 		logger.Err(ctx, err).Msg("Error reading input parameters.")
 		return 0
 	}
 
-	vars := make(map[string]any)
-	if err := utils.JsonDeserialize([]byte(sVars), &vars); err != nil {
-		logger.Err(ctx, err).Msg("Error deserializing GraphQL variables.")
-		return 0
-	}
-
-	host, err := hosts.GetHttpHost(hostName)
-	if err != nil {
-		logger.Err(ctx, err).Msg("Error getting host.")
-		return 0
-	}
-
-	result, err := executeGraphql(ctx, host, stmt, vars)
+	result, err := graphqlclient.Execute(ctx, hostName, stmt, varsJson)
 	if err != nil {
 		logger.Err(ctx, err).Msg("Error executing GraphQL operation.")
 		return 0
@@ -51,37 +35,4 @@ func hostExecuteGQL(ctx context.Context, mod wasm.Module, pHostName uint32, pStm
 	}
 
 	return offset
-}
-
-type graphqlRequest struct {
-	Query     string         `json:"query"`
-	Variables map[string]any `json:"variables"`
-}
-
-func executeGraphql(ctx context.Context, host manifest.HTTPHostInfo, stmt string, vars map[string]any) (string, error) {
-	// https://graphql.org/learn/serving-over-http/
-	requestPayload := graphqlRequest{
-		Query:     stmt,
-		Variables: vars,
-	}
-
-	result, err := hosts.PostToHostEndpoint[[]byte](ctx, host, requestPayload)
-	if err != nil {
-		return "", fmt.Errorf("error posting GraphQL statement: %w", err)
-	}
-
-	// Check for errors in the response so we can log them.
-	response := result.Data
-	gqlErrors, dataType, _, err := jsonparser.Get(response, "errors")
-	if err != nil && err != jsonparser.KeyPathNotFoundError {
-		return "", fmt.Errorf("error parsing GraphQL response: %w", err)
-	}
-	if dataType == jsonparser.Array && len(gqlErrors) > 0 {
-		logger.Warn(ctx).
-			Bool("user_visible", true).
-			Str("errors", string(gqlErrors)).
-			Msg("GraphQL API call returned errors.")
-	}
-
-	return string(response), nil
 }
