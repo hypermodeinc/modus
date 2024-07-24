51a286cda5b126e7c4a646e18e951737b1a86fe5
Matt Johnson-Pint <mjp@hypermode.com>
2024-07-24 13:40:19 -0700

    Refactor models host functions

--- models/models.go
+++ models/models.go
@@ -20,16 +20,6 @@ import (
 	"github.com/hypermodeAI/manifest"
 )
 
-// generic output format for models functions
-// can be extended to support more formats
-// for now, we support text and json_object used in generateText function
-type OutputFormat string
-
-const (
-	OutputFormatText OutputFormat = "text"
-	OutputFormatJson OutputFormat = "json_object"
-)
-
 func GetModel(modelName string) (manifest.ModelInfo, error) {
 	model, ok := manifestdata.Manifest.Models[modelName]
 	if !ok {
@@ -37,6 +27,64 @@ func GetModel(modelName string) (manifest.ModelInfo, error) {
 	}
 
 	return model, nil
+}
+
+func GetModelInfo(modelName string) (*ModelInfo, error) {
+	model, err := GetModel(modelName)
+	if err != nil {
+		return nil, err
+	}
+
+	info := &ModelInfo{
+		Name:     model.Name,
+		FullName: model.SourceModel,
+	}
+
+	return info, nil
+}
+
+func InvokeModel(ctx context.Context, modelName string, input string) (string, error) {
+	model, err := GetModel(modelName)
+	if err != nil {
+		return "", err
+	}
+
+	// TODO: use the provider pattern instead of branching
+	if model.Host == "aws-bedrock" {
+		return invokeAwsBedrockModel(ctx, model, input)
+	}
+
+	return postToModelEndpoint[string](ctx, model, input)
+}
+
+func postToModelEndpoint[TResult any](ctx context.Context, model manifest.ModelInfo, payload any) (TResult, error) {
+	span := utils.NewSentrySpanForCurrentFunc(ctx)
+	defer span.Finish()
+
+	endpoint, host, err := getModelEndpointAndHost(model)
+	if err != nil {
+		var empty TResult
+		return empty, err
+	}
+
+	bs := func(ctx context.Context, req *http.Request) error {
+		req.Header.Set("Content-Type", "application/json")
+		if host.Name != hosts.HypermodeHost {
+			return secrets.ApplyHostSecretsToHttpRequest(ctx, host, req)
+		}
+
+		return nil
+	}
+
+	res, err := utils.PostHttp[TResult](ctx, endpoint, payload, bs)
+	if err != nil {
+		var empty TResult
+		return empty, err
+	}
+
+	db.WriteInferenceHistory(ctx, model, payload, res.Data, res.StartTime, res.EndTime)
+
+	return res.Data, nil
 }
 
 func getModelEndpointAndHost(model manifest.ModelInfo) (string, manifest.HTTPHostInfo, error) {
@@ -70,94 +118,4 @@ func getModelEndpointAndHost(model manifest.ModelInfo) (string, manifest.HTTPHos
 	}
 
 	return host.Endpoint, host, nil
-}
-
-func PostToModelEndpoint[TResult any](ctx context.Context, model manifest.ModelInfo, payload any) (TResult, error) {
-	span := utils.NewSentrySpanForCurrentFunc(ctx)
-	defer span.Finish()
-
-	endpoint, host, err := getModelEndpointAndHost(model)
-	if err != nil {
-		var empty TResult
-		return empty, err
-	}
-
-	bs := func(ctx context.Context, req *http.Request) error {
-		req.Header.Set("Content-Type", "application/json")
-		if host.Name != hosts.HypermodeHost {
-			return secrets.ApplyHostSecretsToHttpRequest(ctx, host, req)
-		}
-
-		return nil
-	}
-
-	res, err := utils.PostHttp[TResult](ctx, endpoint, payload, bs)
-	if err != nil {
-		var empty TResult
-		return empty, err
-	}
-
-	db.WriteInferenceHistory(ctx, model, payload, res.Data, res.StartTime, res.EndTime)
-
-	return res.Data, nil
-}
-
-func PostToModelEndpoint_Old[TResult any](ctx context.Context, sentenceMap map[string]string, model manifest.ModelInfo) (map[string]TResult, error) {
-	span := utils.NewSentrySpanForCurrentFunc(ctx)
-	defer span.Finish()
-
-	// self hosted models takes in array, can optimize for parallelizing later
-	keys, sentences := []string{}, []string{}
-
-	for k, v := range sentenceMap {
-		// create a map of sentences to send to the model
-		sentences = append(sentences, v)
-		// create a list of keys to map the results back to the original sentences
-		keys = append(keys, k)
-	}
-	// create a map of sentences to send to the model
-	req := map[string][]string{"instances": sentences}
-
-	res, err := PostToModelEndpoint[PredictionResult[TResult]](ctx, model, req)
-	if err != nil {
-		return nil, err
-	}
-
-	if len(res.Predictions) != len(keys) {
-		return nil, fmt.Errorf("number of predictions does not match number of sentences")
-	}
-
-	// map the results back to the original sentences
-	result := make(map[string]TResult)
-	for i, v := range res.Predictions {
-		result[keys[i]] = v
-	}
-
-	return result, nil
-}
-
-type PredictionResult[T any] struct {
-	Predictions []T `json:"predictions"`
-}
-
-// Define  structures used by text generation functions
-type ChatMessage struct {
-	Role    string `json:"role"`
-	Content string `json:"content"`
-}
-
-type ChatResponse struct {
-	Choices []MessageChoice `json:"choices"`
-	Error   InvokeError     `json:"error"`
-}
-
-type MessageChoice struct {
-	Message ChatMessage `json:"message"`
-}
-
-type InvokeError struct {
-	Message string `json:"message"`
-	Type    string `json:"type"`
-	Param   string `json:"param"`
-	Code    string `json:"code"`
 }
