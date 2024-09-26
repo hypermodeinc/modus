a48e7be304d4b086277a7756d85248647dfc8fd6
Matt Johnson-Pint <mjp@hypermode.com>
2024-09-26 15:50:14 -0700

    Improve local debugging

--- hostfunctions/log.go
+++ hostfunctions/log.go
@@ -6,6 +6,8 @@ package hostfunctions
 
 import (
 	"context"
+	"fmt"
+	"os"
 
 	"hypruntime/logger"
 	"hypruntime/utils"
@@ -17,17 +19,23 @@ func init() {
 
 func LogFunctionMessage(ctx context.Context, level, message string) {
 
+	// store messages in the context, so we can return them to the caller
+	messages := ctx.Value(utils.FunctionMessagesContextKey).(*[]utils.LogMessage)
+	*messages = append(*messages, utils.LogMessage{
+		Level:   level,
+		Message: message,
+	})
+
+	// If debugging, write debug messages to stderr instead of the logger
+	if level == "debug" && utils.HypermodeDebugEnabled() {
+		fmt.Fprintln(os.Stderr, message)
+		return
+	}
+
 	// write to the logger
 	logger.Get(ctx).
 		WithLevel(logger.ParseLevel(level)).
 		Str("text", message).
 		Bool("user_visible", true).
 		Msg("Message logged from function.")
-
-	// also store messages in the context, so we can return them to the caller
-	messages := ctx.Value(utils.FunctionMessagesContextKey).(*[]utils.LogMessage)
-	*messages = append(*messages, utils.LogMessage{
-		Level:   level,
-		Message: message,
-	})
 }
