462be6d1a1b23801326e22edd5854ce9eba5678d
Matt Johnson-Pint <mjp@hypermode.com>
2024-10-03 15:54:31 -0700

    Update copyright headers

--- sdk/go/pkg/db/imports_mock.go
+++ sdk/go/pkg/db/imports_mock.go
@@ -2,6 +2,11 @@
 
 /*
  * Copyright 2024 Hypermode, Inc.
+ * Licensed under the terms of the Apache License, Version 2.0
+ * See the LICENSE file that accompanied this code for further details.
+ *
+ * SPDX-FileCopyrightText: 2024 Hypermode, Inc. <hello@hypermode.com>
+ * SPDX-License-Identifier: Apache-2.0
  */
 
 package db
