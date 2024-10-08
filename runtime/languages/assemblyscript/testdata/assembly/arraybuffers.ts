01c534bd18ecdedfebb14d353afbef65b48b32ad
Matt Johnson-Pint <mjp@hypermode.com>
2024-10-07 17:19:50 -0700

    Remove extra comma

--- runtime/languages/assemblyscript/testdata/assembly/arraybuffers.ts
+++ runtime/languages/assemblyscript/testdata/assembly/arraybuffers.ts
@@ -1,27 +1,27 @@
 /*
- * Copyright 2024 Hypermode, Inc.
+ * Copyright 2024 Hypermode Inc.
  * Licensed under the terms of the Apache License, Version 2.0
  * See the LICENSE file that accompanied this code for further details.
  *
- * SPDX-FileCopyrightText: 2024 Hypermode, Inc. <hello@hypermode.com>
+ * SPDX-FileCopyrightText: 2024 Hypermode Inc. <hello@hypermode.com>
  * SPDX-License-Identifier: Apache-2.0
  */
 
 export function testArrayBufferInput(buffer: ArrayBuffer): void {
-    const view = Uint8Array.wrap(buffer);
-    assert(view.length == 4);
-    assert(view[0] == 1);
-    assert(view[1] == 2);
-    assert(view[2] == 3);
-    assert(view[3] == 4);
+  const view = Uint8Array.wrap(buffer);
+  assert(view.length == 4);
+  assert(view[0] == 1);
+  assert(view[1] == 2);
+  assert(view[2] == 3);
+  assert(view[3] == 4);
 }
 
 export function testArrayBufferOutput(): ArrayBuffer {
-    const buffer = new ArrayBuffer(4);
-    const view = Uint8Array.wrap(buffer);
-    view[0] = 1;
-    view[1] = 2;
-    view[2] = 3;
-    view[3] = 4;
-    return buffer;
+  const buffer = new ArrayBuffer(4);
+  const view = Uint8Array.wrap(buffer);
+  view[0] = 1;
+  view[1] = 2;
+  view[2] = 3;
+  view[3] = 4;
+  return buffer;
 }
--- runtime/languages/assemblyscript/testdata/assembly/strings.ts
+++ runtime/languages/assemblyscript/testdata/assembly/strings.ts
@@ -1,51 +1,51 @@
 /*
- * Copyright 2024 Hypermode, Inc.
+ * Copyright 2024 Hypermode Inc.
  * Licensed under the terms of the Apache License, Version 2.0
  * See the LICENSE file that accompanied this code for further details.
  *
- * SPDX-FileCopyrightText: 2024 Hypermode, Inc. <hello@hypermode.com>
+ * SPDX-FileCopyrightText: 2024 Hypermode Inc. <hello@hypermode.com>
  * SPDX-License-Identifier: Apache-2.0
  */
 
 // "Hello World" in Japanese
-const testString = "こんにちは、世界"
+const testString = "こんにちは、世界";
 
 export function testStringInput(s: string): void {
-    assert(s == testString);
+  assert(s == testString);
 }
 
 export function testStringOutput(): string {
-    return testString;
+  return testString;
 }
 
 export function testStringInput_empty(s: string): void {
-    assert(s == "");
+  assert(s == "");
 }
 
 export function testStringOutput_empty(): string {
-    return "";
+  return "";
 }
 
 export function testNullStringInput(s: string | null): void {
-    assert(s == testString);
+  assert(s == testString);
 }
 
 export function testNullStringOutput(): string | null {
-    return testString;
+  return testString;
 }
 
 export function testNullStringInput_empty(s: string | null): void {
-    assert(s == "");
+  assert(s == "");
 }
 
 export function testNullStringOutput_empty(): string | null {
-    return "";
+  return "";
 }
 
 export function testNullStringInput_null(s: string | null): void {
-    assert(s == null);
+  assert(s == null);
 }
 
 export function testNullStringOutput_null(): string | null {
-    return null;
+  return null;
 }
