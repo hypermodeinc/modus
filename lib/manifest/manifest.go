f6a7147a7a8da25a5e5cebf36a3ef188745f6d1b
Jai Radhakrishnan <55522316+jairad26@users.noreply.github.com>
2024-10-16 12:10:43 -0700

    update manifest version to 3 to maintain with hypermode console (#471)

--- lib/manifest/manifest.go
+++ lib/manifest/manifest.go
@@ -23,7 +23,8 @@ import (
 // to the manifest schema.  In general, we don't consider the schema to be versioned,
 // from the user's perspective, so this should be rare.
 // NOTE: We intentionally do not expose the *current* version number outside this package.
-const currentVersion = 1
+// NOTE: versions 1 & 2 are reserved for the prior hypermode.json, modus.json starts at version 3.
+const currentVersion = 3
 
 //go:embed modus_schema.json
 var schemaContent string
