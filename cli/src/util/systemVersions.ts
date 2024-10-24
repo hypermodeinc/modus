8449aaf2afc1dd981a5cbdf542bdcbc33cb17602
Pine <octref@gmail.com>
2024-10-24 20:59:46 +0200

    fix copyright headers

--- cli/src/util/systemVersions.ts
+++ cli/src/util/systemVersions.ts
@@ -1,13 +1,24 @@
+/*
+ * Copyright 2024 Hypermode Inc.
+ * Licensed under the terms of the Apache License, Version 2.0
+ * See the LICENSE file that accompanied this code for further details.
+ *
+ * SPDX-FileCopyrightText: 2024 Hypermode Inc. <hello@hypermode.com>
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 import { execFile } from "./cp.js";
 import { ModusHomeDir } from "../custom/globals.js";
 
+const EXEC_OPTIONS = {
+  shell: true,
+  cwd: ModusHomeDir,
+  env: process.env,
+};
+
 export async function getGoVersion(): Promise<string | undefined> {
   try {
-    const result = await execFile("go", ["version"], {
-      shell: true,
-      cwd: ModusHomeDir,
-      env: process.env,
-    });
+    const result = await execFile("go", ["version"], EXEC_OPTIONS);
     const parts = result.stdout.split(" ");
     const str = parts.length > 2 ? parts[2] : undefined;
     if (str?.startsWith("go")) {
@@ -18,11 +29,7 @@ export async function getGoVersion(): Promise<string | undefined> {
 
 export async function getTinyGoVersion(): Promise<string | undefined> {
   try {
-    const result = await execFile("tinygo", ["version"], {
-      shell: true,
-      cwd: ModusHomeDir,
-      env: process.env,
-    });
+    const result = await execFile("tinygo", ["version"], EXEC_OPTIONS);
     const parts = result.stdout.split(" ");
     return parts.length > 2 ? parts[2] : undefined;
   } catch {}
@@ -30,7 +37,7 @@ export async function getTinyGoVersion(): Promise<string | undefined> {
 
 export async function getNPMVersion(): Promise<string | undefined> {
   try {
-    const result = await execFile("npm", ["--version"], { shell: true });
+    const result = await execFile("npm", ["--version"], EXEC_OPTIONS);
     const parts = result.stdout.split(" ");
     return parts.length > 0 ? parts[0].trim() : undefined;
   } catch {}
