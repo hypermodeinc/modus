9c2c190db5482b4dbe823566b72a7a050267ea37
Kevin Mingtarja <kevinm@hypermode.com>
2024-11-04 23:42:31 +0700

    use async writeFile function

--- tools/release-info/index.ts
+++ tools/release-info/index.ts
@@ -14,43 +14,43 @@ import {
   getAllSdkVersions,
   getAllRuntimeVersions,
   getAllCliVersions,
-} from "./versioninfo.js";
-import { SDK } from "./constants.js";
-import { writeFileSync } from "fs";
+} from "./versioninfo.js"
+import { SDK } from "./constants.js"
+import { writeFile } from "fs/promises"
 
 async function buildModusLatestJSON(
   filename: string,
-  includePrerelease: boolean,
+  includePrerelease: boolean
 ) {
-  const as = await getLatestSdkVersion(SDK.AssemblyScript, includePrerelease);
-  const go = await getLatestSdkVersion(SDK.Go, includePrerelease);
-  const cli = await getLatestCliVersion(includePrerelease);
-  const runtime = await getLatestRuntimeVersion(includePrerelease);
+  const as = await getLatestSdkVersion(SDK.AssemblyScript, includePrerelease)
+  const go = await getLatestSdkVersion(SDK.Go, includePrerelease)
+  const cli = await getLatestCliVersion(includePrerelease)
+  const runtime = await getLatestRuntimeVersion(includePrerelease)
   const info = {
     "sdk/assemblyscript": as,
     "sdk/go": go,
     cli: cli,
     runtime: runtime,
-  };
+  }
 
-  writeFileSync(filename, JSON.stringify(info, null, 2));
+  await writeFile(filename, JSON.stringify(info, null, 2))
 }
 
 async function buildModusAllJSON(filename: string, includePrerelease: boolean) {
-  const as = await getAllSdkVersions(SDK.AssemblyScript, includePrerelease);
-  const go = await getAllSdkVersions(SDK.Go, includePrerelease);
-  const cli = await getAllCliVersions(includePrerelease);
-  const runtime = await getAllRuntimeVersions(includePrerelease);
+  const as = await getAllSdkVersions(SDK.AssemblyScript, includePrerelease)
+  const go = await getAllSdkVersions(SDK.Go, includePrerelease)
+  const cli = await getAllCliVersions(includePrerelease)
+  const runtime = await getAllRuntimeVersions(includePrerelease)
   const info = {
     "sdk/assemblyscript": as,
     "sdk/go": go,
     cli: cli,
     runtime: runtime,
-  };
-  writeFileSync(filename, JSON.stringify(info, null, 2));
+  }
+  await writeFile(filename, JSON.stringify(info, null, 2))
 }
 
-await buildModusLatestJSON("modus-latest.json", false);
-await buildModusLatestJSON("modus-preview.json", true);
-await buildModusAllJSON("modus-all.json", false);
-await buildModusAllJSON("modus-preview-all.json", true);
+await buildModusLatestJSON("modus-latest.json", false)
+await buildModusLatestJSON("modus-preview.json", true)
+await buildModusAllJSON("modus-all.json", false)
+await buildModusAllJSON("modus-preview-all.json", true)
