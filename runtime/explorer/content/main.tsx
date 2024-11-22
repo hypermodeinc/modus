b2ba4f7ddca79e8fcaee032503605f4f2e69e277
Kristen Cheung <krcheung9@gmail.com>
2024-11-22 15:31:30 -0800

    add modus specific webpage

--- runtime/explorer/content/main.tsx
+++ runtime/explorer/content/main.tsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { createRoot } from "react-dom/client";
 import { ApiExplorer } from "@hypermode/react-api-explorer";
+import ModusIcon from "./ModusIcon";
 import "@hypermode/react-api-explorer/styles.css";
+import "./index.css";
 
 const rootElement = document.getElementById("root");
 if (!rootElement) {
@@ -15,14 +17,14 @@ function App() {
     foreground: "0 0% 100%",
     card: "150 55% 8%",
     "card-foreground": "0 0% 100%",
-    popover: "150 55% 8%",
+    popover: "150 60% 4%",
     "popover-foreground": "0 0% 100%",
     primary: "150 60% 39%",
     "primary-foreground": "0 0% 100%",
     secondary: "157 73% 57%",
     "secondary-foreground": "150 60% 3%",
     muted: "200 15% 12%",
-    "muted-foreground": "150 15% 60%",
+    "muted-foreground": "200 8% 64%",
     accent: "150 35% 17%",
     "accent-foreground": "157 73% 57%",
     destructive: "0 84% 60%",
@@ -58,7 +60,17 @@ function App() {
     fetchEndpoints();
   }, []);
 
-  return <ApiExplorer endpoints={endpoints} theme={modusTheme} />;
+  return (
+    <div className="bg-black pb-2 px-2 h-screen flex flex-col">
+      <div className="w-full flex items-center justify-center p-2 text-sm text-white/60 tracking-wide">
+        <p>Powered by Modus</p>
+        <ModusIcon className="w-8 h-8 mt-1 -ml-1" />
+      </div>
+      <div className="grow rounded-lg">
+        <ApiExplorer endpoints={endpoints} theme={modusTheme} />
+      </div>
+    </div>
+  );
 }
 
 root.render(<App />);
