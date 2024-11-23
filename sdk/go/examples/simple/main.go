7f77e80df3b3285985668abd36eec12f0365acfe
Matt Johnson-Pint <mjp@hypermode.com>
2024-11-23 11:38:15 -0800

    Adjust comments in simple examples

--- sdk/go/examples/simple/main.go
+++ sdk/go/examples/simple/main.go
@@ -16,14 +16,18 @@ import (
 	"github.com/hypermodeinc/modus/sdk/go/pkg/console"
 )
 
+// Logs a message.
 func LogMessage(message string) {
 	console.Log(message)
 }
 
+// Adds two integers together and returns the result.
 func Add(x, y int) int {
 	return x + y
 }
 
+// Adds three integers together and returns the result.
+// The third integer is optional.
 func Add3(a, b int, c *int) int {
 	if c != nil {
 		return a + b + *c
@@ -31,6 +35,7 @@ func Add3(a, b int, c *int) int {
 	return a + b
 }
 
+// Adds any number of integers together and returns the result.
 func AddN(args ...int) int {
 	sum := 0
 	for _, arg := range args {
@@ -42,18 +47,23 @@ func AddN(args ...int) int {
 // this indirection is so we can mock time.Now in tests
 var nowFunc = time.Now
 
+// Returns the current time.
 func GetCurrentTime() time.Time {
 	return nowFunc()
 }
 
+// Returns the current time formatted as a string.
 func GetCurrentTimeFormatted() string {
 	return nowFunc().Format(time.DateTime)
 }
 
+// Combines the first and last name of a person, and returns the full name.
 func GetFullName(firstName, lastName string) string {
 	return firstName + " " + lastName
 }
 
+// Says hello to a person by name.
+// If the name is not provided, it will say hello without a name.
 func SayHello(name *string) string {
 	if name == nil {
 		return "Hello!"
@@ -62,12 +72,20 @@ func SayHello(name *string) string {
 	}
 }
 
+// A simple object representing a person.
 type Person struct {
+
+	// The person's first name.
 	FirstName string `json:"firstName"`
-	LastName  string `json:"lastName"`
-	Age       int    `json:"age"`
+
+	// The person's last name.
+	LastName string `json:"lastName"`
+
+	// The person's age.
+	Age int `json:"age"`
 }
 
+// Gets a person object.
 func GetPerson() Person {
 	return Person{
 		FirstName: "John",
@@ -76,12 +94,14 @@ func GetPerson() Person {
 	}
 }
 
+// Gets a random person object from a list of people.
 func GetRandomPerson() Person {
 	people := GetPeople()
 	i := rand.Intn(len(people))
 	return people[i]
 }
 
+// Gets a list of people.
 func GetPeople() []Person {
 	return []Person{
 		{
@@ -102,17 +122,21 @@ func GetPeople() []Person {
 	}
 }
 
+// Gets the name and age of a person.
 func GetNameAndAge() (name string, age int) {
 	p := GetPerson()
 	return GetFullName(p.FirstName, p.LastName), p.Age
 }
 
-// This is the preferred way to handle errors in functions.
-// Simply declare an error interface as the last return value.
-// You can use any object that implements the Go error interface.
-// For example, you can create a new error with errors.New("message"),
-// or with fmt.Errorf("message with %s", "parameters").
+// Tests returning an error.
 func TestNormalError(input string) (string, error) {
+
+	// This is the preferred way to handle errors in functions.
+	// Simply declare an error interface as the last return value.
+	// You can use any object that implements the Go error interface.
+	// For example, you can create a new error with errors.New("message"),
+	// or with fmt.Errorf("message with %s", "parameters").
+
 	if input == "" {
 		return "", errors.New("input is empty")
 	}
@@ -120,9 +144,12 @@ func TestNormalError(input string) (string, error) {
 	return output, nil
 }
 
-// This is an alternative way to handle errors in functions.
-// It is identical in behavior to TestNormalError, but is not Go idiomatic.
+// Tests an alternative way to handle errors in functions.
 func TestAlternativeError(input string) string {
+
+	// This is an alternative way to handle errors in functions.
+	// It is identical in behavior to TestNormalError, but is not Go idiomatic.
+
 	if input == "" {
 		console.Error("input is empty")
 		return ""
@@ -131,22 +158,29 @@ func TestAlternativeError(input string) string {
 	return output
 }
 
-// This panics, will log the message as "fatal" and exits the function.
-// Generally, you should not panic.
+// Tests a panic.
 func TestPanic() {
+
+	// This panics, will log the message as "fatal" and exits the function.
+	// Generally, you should not panic.
+
 	panic("This is a message from a panic.\nThis is a second line from a panic.\n")
 }
 
-// If you need to exit prematurely without panicking, you can use os.Exit.
-// However, you cannot return any values from the function, so if you want
-// to log an error message, you should do so before calling os.Exit.
-// The exit code should be 0 for success, and non-zero for failure.
+// Tests an exit with a non-zero exit code.
 func TestExit() {
+
+	// If you need to exit prematurely without panicking, you can use os.Exit.
+	// However, you cannot return any values from the function, so if you want
+	// to log an error message, you should do so before calling os.Exit.
+	// The exit code should be 0 for success, and non-zero for failure.
+
 	console.Error("This is an error message.")
 	os.Exit(1)
 	println("This line will not be executed.")
 }
 
+// Tests logging at different levels.
 func TestLogging() {
 	// This is a simple log message. It has no level.
 	console.Log("This is a simple log message.")
