9987f6c8443782b0b0a04f6840f0cffde8c1646d
Matt Johnson-Pint <mjp@hypermode.com>
2024-12-16 15:03:11 -0800

    .

--- runtime/utils/console_test.go
+++ runtime/utils/console_test.go
@@ -10,9 +10,11 @@
 package utils_test
 
 import (
+	"bytes"
 	"testing"
 
 	"github.com/hypermodeinc/modus/runtime/utils"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestIsError(t *testing.T) {
@@ -55,5 +57,58 @@ func TestSplitConsoleOutputLine(t *testing.T) {
 		if level != test.expectedLevel || message != test.expectedMessage {
 			t.Errorf("For input '%s', expected level '%s' and message '%s', but got level '%s' and message '%s'", test.input, test.expectedLevel, test.expectedMessage, level, message)
 		}
+	}
+}
+
+type mockOutputBuffers struct {
+	stdOut *bytes.Buffer
+	stdErr *bytes.Buffer
+}
+
+func (m mockOutputBuffers) StdOut() *bytes.Buffer {
+	return m.stdOut
+}
+
+func (m mockOutputBuffers) StdErr() *bytes.Buffer {
+	return m.stdErr
+}
+
+func TestTransformConsoleOutput(t *testing.T) {
+	tests := []struct {
+		stdOut   string
+		stdErr   string
+		expected []utils.LogMessage
+	}{
+		{
+			stdOut:   "",
+			stdErr:   "",
+			expected: []utils.LogMessage{},
+		},
+		{
+			stdOut: "Info: This is an info message\n",
+			stdErr: "Error: This is an error message\n",
+			expected: []utils.LogMessage{
+				{Level: "info", Message: "This is an info message"},
+				{Level: "error", Message: "This is an error message"},
+			},
+		},
+		{
+			stdOut: "Debug: This is a debug message\nWarning: This is a warning message\n",
+			stdErr: "panic: This is a fatal message\n",
+			expected: []utils.LogMessage{
+				{Level: "debug", Message: "This is a debug message"},
+				{Level: "warning", Message: "This is a warning message"},
+				{Level: "fatal", Message: "This is a fatal message"},
+			},
+		},
+	}
+
+	for _, test := range tests {
+		buffers := mockOutputBuffers{
+			stdOut: bytes.NewBufferString(test.stdOut),
+			stdErr: bytes.NewBufferString(test.stdErr),
+		}
+		result := utils.TransformConsoleOutput(buffers)
+		assert.Equal(t, test.expected, result)
 	}
 }
