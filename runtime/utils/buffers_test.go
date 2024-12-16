67ec310d44db6bb6a107dcf1ada3b83864e1bd84
Matt Johnson-Pint <mjp@hypermode.com>
2024-12-16 14:11:15 -0800

    .

--- runtime/utils/buffers_test.go
+++ runtime/utils/buffers_test.go
@@ -10,19 +10,32 @@
 package utils_test
 
 import (
+	"os"
 	"testing"
 
 	"github.com/hypermodeinc/modus/runtime/utils"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestNewOutputBuffers(t *testing.T) {
 	buffers := utils.NewOutputBuffers()
 
-	if buffers.StdOut() == nil {
-		t.Errorf("Expected StdOut buffer to be initialized, but got nil")
-	}
+	outStream := buffers.StdOut()
+	errStream := buffers.StdErr()
 
-	if buffers.StdErr() == nil {
-		t.Errorf("Expected StdErr buffer to be initialized, but got nil")
-	}
+	assert.NotNil(t, outStream)
+	assert.NotNil(t, errStream)
+
+	assert.NotSame(t, outStream, errStream)
+	assert.NotSame(t, os.Stdout, outStream)
+	assert.NotSame(t, os.Stderr, errStream)
+
+	assert.Equal(t, 0, outStream.Cap())
+	assert.Equal(t, 0, errStream.Cap())
+
+	outStream.Write([]byte("Hello, World!"))
+	errStream.Write([]byte("Hello, Error!"))
+
+	assert.Equal(t, "Hello, World!", outStream.String())
+	assert.Equal(t, "Hello, Error!", errStream.String())
 }
