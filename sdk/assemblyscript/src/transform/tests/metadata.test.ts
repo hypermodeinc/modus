86cbe328c60cdf6177b400a9610eca9a77e0ddf9
Jairus <me@jairus.dev>
2024-12-23 14:05:31 -0500

    add license

--- sdk/assemblyscript/src/transform/tests/metadata.test.ts
+++ sdk/assemblyscript/src/transform/tests/metadata.test.ts
@@ -1,3 +1,11 @@
+/*
+ * Copyright 2024 Hypermode Inc.
+ * Licensed under the terms of the Apache License, Version 2.0
+ * See the LICENSE file that accompanied this code for further details.
+ *
+ * SPDX-FileCopyrightText: 2024 Hypermode Inc. <hello@hypermode.com>
+ * SPDX-License-Identifier: Apache-2.0
+ */
 /* eslint-disable no-undef */
 import test from "node:test";
 import binaryen from "assemblyscript/lib/binaryen.js";
