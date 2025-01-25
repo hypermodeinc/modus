8b2a28633e42bb955675023bd2505759d236d811
Matt Johnson-Pint <mjp@hypermode.com>
2025-01-24 17:09:03 -0800

    fix: separate modusdb instance by app

--- runtime/db/modusdb.go
+++ runtime/db/modusdb.go
@@ -10,7 +10,10 @@
 package db
 
 import (
+	"bufio"
 	"context"
+	"errors"
+	"os"
 	"path/filepath"
 	"runtime"
 
@@ -22,18 +25,70 @@ import (
 var GlobalModusDbEngine *modusdb.Engine
 
 func InitModusDb(ctx context.Context) {
-	if app.IsDevEnvironment() && runtime.GOOS != "windows" {
-		dataDir := filepath.Join(app.ModusHomeDir(), "data")
-		if eng, err := modusdb.NewEngine(modusdb.NewDefaultConfig(dataDir)); err != nil {
-			logger.Fatal(ctx).Err(err).Msg("Failed to initialize modusdb.")
-		} else {
-			GlobalModusDbEngine = eng
-		}
+	if !app.IsDevEnvironment() || runtime.GOOS == "windows" {
+		// ModusDB should only be initialized in dev environment,
+		// and currently does not work on Windows.
+		return
+	}
+
+	var dataDir string
+	appPath := app.Config().AppPath()
+	if filepath.Base(appPath) == "build" {
+		// this keeps the data directory outside of the build directory
+		dataDir = filepath.Join(appPath, "..", ".modusdb")
+		addToGitIgnore(ctx, filepath.Dir(appPath))
+	} else {
+		dataDir = filepath.Join(appPath, ".modusdb")
+	}
+
+	if eng, err := modusdb.NewEngine(modusdb.NewDefaultConfig(dataDir)); err != nil {
+		logger.Fatal(ctx).Err(err).Msg("Failed to initialize modusdb.")
+	} else {
+		GlobalModusDbEngine = eng
 	}
 }
 
 func CloseModusDb(ctx context.Context) {
 	if GlobalModusDbEngine != nil {
 		GlobalModusDbEngine.Close()
+	}
+}
+
+func addToGitIgnore(ctx context.Context, rootPath string) {
+	gitIgnorePath := filepath.Join(rootPath, ".gitignore")
+	gitIgnoreContents := ".modusdb/"
+
+	// if .gitignore file does not exist, create it and add .modusdb/ to it
+	if _, err := os.Stat(gitIgnorePath); errors.Is(err, os.ErrNotExist) {
+		if err := os.WriteFile(gitIgnorePath, []byte(gitIgnoreContents+"\n"), 0644); err != nil {
+			logger.Err(ctx, err).Msg("Failed to create .gitignore file.")
+		}
+		return
+	}
+
+	// check if .modusdb/ is already in the .gitignore file
+	file, err := os.Open(gitIgnorePath)
+	if err != nil {
+		logger.Err(ctx, err).Msg("Failed to open .gitignore file.")
+		return
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		if scanner.Text() == gitIgnoreContents {
+			// found .modusdb/ in the file
+			return
+		}
+	}
+
+	// .modusdb/ is not in the file, so append it
+	file, err = os.OpenFile(gitIgnorePath, os.O_APPEND|os.O_WRONLY, 0644)
+	if err != nil {
+		logger.Err(ctx, err).Msg("Failed to open .gitignore file.")
+		return
+	}
+	defer file.Close()
+	if _, err := file.WriteString("\n" + gitIgnoreContents + "\n"); err != nil {
+		logger.Err(ctx, err).Msg("Failed to append .modusdb/ to .gitignore file.")
 	}
 }
