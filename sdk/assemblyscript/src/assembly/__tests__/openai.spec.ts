a44ae5014b34edf824b40c281d30858b45c0ed62
Matt Johnson-Pint <mjp@hypermode.com>
2025-05-21 15:47:18 -0700

    Implement parseMessages for AssemblyScript

---
+++ sdk/assemblyscript/src/assembly/__tests__/openai.spec.ts
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2025 Hypermode Inc.
+ * Licensed under the terms of the Apache License, Version 2.0
+ * See the LICENSE file that accompanied this code for further details.
+ *
+ * SPDX-FileCopyrightText: 2025 Hypermode Inc. <hello@hypermode.com>
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { expect, it, log, run } from "as-test";
+import { JSON } from "json-as";
+
+import {
+  SystemMessage,
+  UserMessage,
+  AssistantMessage,
+  RequestMessage,
+  parseMessages,
+} from "../../models/openai/chat";
+
+it("should round-trip chat messages", () => {
+  const msgs: RequestMessage[] = [
+    new SystemMessage("You are a helpful assistant."),
+    new UserMessage("What is the capital of France?"),
+    new AssistantMessage("The capital of France is Paris."),
+  ];
+
+  const data = JSON.stringify(msgs);
+  const parsedMsgs = parseMessages(data);
+
+  expect(parsedMsgs.length).toBe(msgs.length);
+  for (let i = 0; i < msgs.length; i++) {
+    expect(msgs[i].role).toBe(parsedMsgs[i].role);
+  }
+
+  const roundTrip = JSON.stringify(parsedMsgs);
+
+  log(data);
+  log(roundTrip);
+  expect(roundTrip).toBe(data); // <-----    this currently fails
+});
+
+run();
