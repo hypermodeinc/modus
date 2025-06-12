48452def6069013fc2de4cb2a0f9ac794bdc96c5
Matt Johnson-Pint <mjp@hypermode.com>
2025-06-11 18:41:59 -0700

    fix deadlock in dynamic mux

--- runtime/httpserver/dynamicMux.go
+++ runtime/httpserver/dynamicMux.go
@@ -10,54 +10,75 @@
 package httpserver
 
 import (
+	"maps"
 	"net/http"
 	"strings"
-	"sync"
+
+	"github.com/puzpuzpuz/xsync/v4"
 )
 
 type dynamicMux struct {
 	mux    *http.ServeMux
-	routes map[string]http.Handler
-	mu     sync.RWMutex
+	routes *xsync.Map[string, http.Handler]
 }
 
 func newDynamicMux(routes map[string]http.Handler) *dynamicMux {
+
+	mapRoutes := xsync.NewMap[string, http.Handler](xsync.WithPresize(len(routes)))
+	for path, handler := range routes {
+		mapRoutes.Store(path, handler)
+	}
+
 	return &dynamicMux{
 		mux:    http.NewServeMux(),
-		routes: routes,
+		routes: mapRoutes,
 	}
 }
 
 func (dm *dynamicMux) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	dm.mu.RLock()
-	defer dm.mu.RUnlock()
 
-	if handler, ok := dm.routes[r.URL.Path]; ok {
+	if handler, ok := dm.routes.Load(r.URL.Path); ok {
 		handler.ServeHTTP(w, r)
 		return
 	}
 
 	path := strings.TrimSuffix(r.URL.Path, "/")
 	if path != r.URL.Path {
-		if _, ok := dm.routes[path]; ok {
+		if _, ok := dm.routes.Load(path); ok {
 			http.Redirect(w, r, path, http.StatusMovedPermanently)
 			return
 		}
 	}
 
-	for route, handler := range dm.routes {
+	var found bool
+	dm.routes.Range(func(route string, handler http.Handler) bool {
 		if len(route) > 1 && strings.HasSuffix(route, "/") &&
 			(strings.HasPrefix(r.URL.Path, route) || route == r.URL.Path+"/") {
 			handler.ServeHTTP(w, r)
-			return
+			found = true
+			return false
+		} else {
+			return true
 		}
-	}
+	})
 
-	http.Error(w, "Not Found", http.StatusNotFound)
+	if !found {
+		http.Error(w, "Not Found", http.StatusNotFound)
+	}
 }
 
 func (dm *dynamicMux) ReplaceRoutes(routes map[string]http.Handler) {
-	dm.mu.Lock()
-	defer dm.mu.Unlock()
-	dm.routes = routes
+	temp := maps.Clone(routes)
+	dm.routes.Range(func(route string, _ http.Handler) bool {
+		if handler, ok := temp[route]; ok {
+			dm.routes.Store(route, handler)
+			delete(temp, route)
+		} else {
+			dm.routes.Delete(route)
+		}
+		return true
+	})
+	for route, handler := range temp {
+		dm.routes.Store(route, handler)
+	}
 }
