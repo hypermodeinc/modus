052861b28dff57f727ef770e21156b712c3620a3
Jairus Tanaka <me@jairus.dev>
2025-07-11 16:35:59 -0700

    feat: add `modus run` command

---
+++ cli/src/commands/run/index.ts
@@ -0,0 +1,48 @@
+/*
+ * SPDX-FileCopyrightText: © Hypermode Inc. <hello@hypermode.com>
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { Args, Flags } from "@oclif/core";
+import { BaseCommand } from "../../baseCommand.js";
+import DevCommand from "../dev/index.js";
+
+export default class RunCommand extends BaseCommand {
+  static args = {
+    path: Args.directory({
+      description: "Path to app directory",
+      default: ".",
+      exists: true,
+    }),
+  };
+
+  static flags = {
+    runtime: Flags.string({
+      char: "r",
+      description: "Modus runtime version to use. If not provided, the latest runtime compatible with the app will be used.",
+    }),
+    prerelease: Flags.boolean({
+      char: "p",
+      aliases: ["pre"],
+      description: "Use a prerelease version of the Modus runtime. Not needed if specifying a runtime version.",
+    }),
+    "no-build": Flags.boolean({
+      aliases: ["nobuild"],
+      description: "Don't build the app before running (or when watching for changes)",
+      default: true,
+    }),
+    watch: Flags.boolean({
+      aliases: ["w"],
+      description: "Watch app code for changes",
+      default: true,
+    }),
+  };
+
+  static description = "Run a Modus app locally";
+
+  static examples = ["modus run", "modus run ./my-app", "modus run ./my-app --no-watch"];
+
+  async run(): Promise<void> {
+    DevCommand.run(this.argv);
+  }
+}
